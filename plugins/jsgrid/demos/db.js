d4957f8c6b0ffd11039efb1d391979a6b32c303a
sandra <sandra@admin-PC>
2019-10-10 11:51:16 +0100

    Initial Commit

---
+++ plugins/jsgrid/demos/db.js
@@ -0,0 +1,52 @@
+(function() {
+
+    window.db = db;
+
+
+    db.lectureMsgs = [
+        {   
+            "date_eval": 0,
+            "niveau_Fr": 3,
+            "comprehension_ques": 4 ,
+            "relanceç_disc": 2 ,
+            "coherence_rep": 4 ,
+            "scenario_repetitif":2,
+            "fait_peur_client": 1,
+            "utilisation_supports":4 ,
+            "rebondir_relance": 3,
+            "proposition_transfo_parachute":2,
+            "mo": 10,
+            "tmr":0.35,
+            "tma":0.00115,
+            "ma":9.18,
+            "pourcent_a3": 0.33,
+            "dollar_photo": 25 ,
+            "transfo":3 ,
+            "commentaires":"amelie6-1904/nathalie17-1908/ponnary1-1809/samuel1-1908/eva5-1903 / reste vague et repète la même chose // insiste pour la transfo // pas de réponse claire// est quand même plus appliquée que sur sms"
+        },  
+        {   
+            "date_eval": 1,
+            "niveau_Fr": 3,
+            "comprehension_ques": 4 ,
+            "relanceç_disc": 2 ,
+            "coherence_rep": 4 ,
+            "scenario_repetitif":2,
+            "fait_peur_client": 1,
+            "utilisation_supports":4 ,
+            "rebondir_relance": 3,
+            "proposition_transfo_parachute":2,
+            "mo": 10,
+            "tmr":0.35,
+            "tma":0.00115,
+            "ma":9.18,
+            "pourcent_a3": 0.33,
+            "dollar_photo": 25 ,
+            "transfo":3 ,
+            "commentaires":"amelie6-1904/nathalie17-1908/ponnary1-1809/samuel1-1908/eva5-1903 / reste vague et repète la même chose // insiste pour la transfo // pas de réponse claire// est quand même plus appliquée que sur sms"
+        }
+        
+       
+         
+    ];
+
+}());
